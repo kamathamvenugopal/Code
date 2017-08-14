2a8b46152e2854f3d126954c5f4084a14232c6bd
kamathamvenugopal <31004112+kamathamvenugopal@users.noreply.github.com>
2017-08-14 16:25:11 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,25 @@
+package com.techm.generalticket.beans;
+
+import java.io.Serializable;
+public class Login implements Serializable {
+	private String userId,password,role;
+
+	
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
